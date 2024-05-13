b008454d1779f837da873a6374a005ba66799639
JunSeob Kim <46683933+BenzeneKim@users.noreply.github.com>
2024-05-13 10:22:05 +0900

    fix: fix some grammar errors

--- Transiver/Transiver/src/main.cpp
+++ Transiver/Transiver/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <stdlib.h>
 
 #define ID 0x00
 #define ALL 0xff
@@ -12,11 +13,14 @@
 #define RGB 0x01
 #define RAINBOW 0x02
 
-
 RF24 radio(7,8); // CE, CSN
-
-const byte address[6] = "000001";
-
+bool SendAck();
+void SendRGB();
+void SendRainbow();
+void Protocol(uint8_t* buf, uint8_t function, uint8_t trans, uint8_t rec, uint8_t* value);
+void FillHeader(uint8_t* protocol, uint8_t function, uint8_t trans, uint8_t rec);
+uint8_t CheckSum(uint8_t* data);
+const uint64_t address = 0x00000000;
 void setup() {
   // Console PC connection
   Serial.begin(115200);
@@ -24,7 +28,7 @@ void setup() {
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MAX);
-  rf.stopListening();
+  radio.stopListening();
 }
 
 void loop() {
@@ -38,53 +42,52 @@ void SendRGB(){
 }
 
 void SendRainbow(){
-  radio.write(&Protocol(RAINBOW, ID, ALL), 7);
+  uint8_t _protocol[7];
+  Protocol(&_protocol[0], RAINBOW, ID, ALL, NULL);
+  radio.write(&_protocol, 7);
 }
 
-byte[] Protocol(byte function, byte trans, byte rec, byte value[]){
+void Protocol(uint8_t* buf, uint8_t function, uint8_t trans, uint8_t rec, uint8_t* value){
   switch(function){
     case 0x00:                                  // ACK
-        byte[7] _protocol;
-        FillHeader(&_protocol, function, trans, rec);
-        _protocol[4] = 0x00;
-        _protocol[5] = 0x00;
-        _protocol[6] = END;
+        FillHeader(buf, function, trans, rec);
+        buf[4] = 0x00;
+        buf[5] = 0x00;
+        buf[6] = END;
       break;
-    case 0x00:                                  // ACK
-        byte[10] _protocol;
-        byte[4] _buf;
-        FillHeader(&_protocol, function, trans, rec);
-        _protocol[4] = 0x03;
+    case 0x01:                                  // RGB
+        uint8_t _buf[4];
+        FillHeader(buf, function, trans, rec);
+        buf[4] = 0x03;
         for(int i=0;i<3;i++){
-          _protocol[i+5] = value[i];
+          buf[i+5] = value[i];
         }
         for(int i=0;i<4;i++){
-          _buf[i] = _protocol[4+i]
+          _buf[i] = buf[4+i];
         }
-        _protocol[5] = CheckSum(&_buf);
-        _protocol[6] = END;
+        buf[5] = CheckSum(&_buf[0]);
+        buf[6] = END;
       break;
-    case 0x02:                                  // ACK
-        byte[7] _protocol;
-        FillHeader(&_protocol, function, trans, rec);
-        _protocol[4] = 0x00;
-        _protocol[5] = 0x00;
-        _protocol[6] = END;
+    case 0x02:                                  // RAINBOW
+        FillHeader(buf, function, trans, rec);
+        buf[4] = 0x00;
+        buf[5] = 0x00;
+        buf[6] = END;
       break;
   }
 }
 
-void FillHeader(byte* protocol, byte function, byte trans, byte rec){
+void FillHeader(uint8_t* protocol, uint8_t function, uint8_t trans, uint8_t rec){
   protocol[0] = START;
   protocol[1] = function;
   protocol[2] = trans;
   protocol[3] = rec;
 }
 
-byte CheckSum(byte* data){
-  byte result = 0x00;
+uint8_t CheckSum(uint8_t* data){
+  uint8_t result = 0x00;
   for(int i=0;i<sizeof(data);i++){
     result += data[i] & 0x01;
   }
-  return byte & 0x01;
+  return result & 0x01;
 }
